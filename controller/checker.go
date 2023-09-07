87f049dfc759e7953200d52aeb48d5e3f3e67474
Misha <zigelboim.misha@gmail.com>
2023-09-07 19:13:59 +0300

    Shift Compressor

---
+++ controller/checker.go
@@ -0,0 +1,7 @@
+package controller
+
+// TODO
+// shiftCompressor to remove multiple occurrences of the key SHIFT.
+// If the key SHIFT is pressed for a long time, multiple events of the
+// key SHIFT will arrive to the pressedKeys slice.
+func shiftCompressor() {}
