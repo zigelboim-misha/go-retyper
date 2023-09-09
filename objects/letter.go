74faa4fe83725097fee0ed985df72d64b03fba30
zigelboim-misha <zigelboim.misha@gmail.com>
2023-09-09 10:51:29 +0300

    Removing AdditionalInfo and IsShift as they are not needed and allocate space

--- objects/letter.go
+++ objects/letter.go
@@ -4,8 +4,6 @@ import "github.com/moutend/go-hook/pkg/types"
 
 // Letter represents the key pressed by the user containing information like was SHIFT pressed.
 type Letter struct {
-	KeyboardEvent  types.KeyboardEvent // The keyboard event containing the pressed key
-	Capitalized    bool                // Was SHIFT pressed? Should the letter be Capitalized?
-	AdditionalInfo string              // Additional info about the key. Should not be empty is event is nil!
-	IsShift        bool                // Was the key pressed SHIFT?
+	KeyboardEvent types.KeyboardEvent // The keyboard event containing the pressed key
+	Capitalized   bool                // Was SHIFT pressed? Should the letter be Capitalized?
 }
