ad6b43689e55491d986d3c8179f5fef9d12c01c5
Zigelboim Misha <31862542+zigelboim-misha@users.noreply.github.com>
2023-09-09 15:37:05 +0300

    feat(controller): Clearing the `keysPressed` slice when conditions are met (#9)
    * Changelog
    
    * Controller is checking for TAB, DELETE and Arrow keys to flush the slice
    
    * Timer to flush the slice was added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# 0.0.1 (2023.09.07)
+# 0.0.1 (2023.09.09)
 
 ## New Features 🚀
 
@@ -10,13 +10,19 @@
 - [#6](https://github.com/zigelboim-misha/go-retyper/pull/6) - [Letter](/objects/letter.go) object was created to
 pass additional information about the pressed keys:
   - [Letter](/objects/letter.go) was added
-  - [Re-Tpyer](/typer/typer.go) now can type capital letters
+  - [Re-Typer](/typer/typer.go) now can type capital letters
   - Keyboard object created in [main.go](/main.go)
   - [Keylogger](/keylogger/keylogger.go) now uses the [Letter](/objects/letter.go) object and checks for capital letters
+
+- [#9](https://github.com/zigelboim-misha/go-retyper/pull/9) - Clearing the `keysPressed` slice when:
+  - `TAB` was pressed - User moved to the next input field on a website
+  - `Up`, `Down`, `Left` and `Right` keys were pressed - User looked at the screen and moved his cross-hair
+  - `Delete` or `Back Space` keys were pressed - User deleted text, he wants to fix the error himself
+  - Timeout - The user left the keyboard for a long time (make it configurable)
 
 ## Fixes 🌌
 
 - [#8](https://github.com/zigelboim-misha/go-retyper/pull/8) - Re-typing more than once results in deleting **all**
 written text and replacing it again:
-  - Stopping the key-logger while the program re-types the wrong inputted letters
+  - Stopping the keylogger while the program re-types the wrong inputted letters
   - [Letter](/objects/letter.go) was deducted of 2 unneeded fields
--- controller/controller.go
+++ controller/controller.go
@@ -6,28 +6,58 @@ import (
 	"keyboard/keylogger"
 	"keyboard/objects"
 	"keyboard/typer"
+	"time"
 )
 
 var (
 	keysPressed       []objects.Letter
 	pressedKeysChan   = make(chan objects.Letter) // Channel for all pressed keys
 	stopKeyloggerChan = make(chan bool)           // Telling the keylogger.KeyLogger() method to stop key-logging
+	resetTimerChan    = make(chan bool)           // Flushing the pressedKeysChan slice nothing is pressed for a while
 )
 
 // Start key-logs the users keyboard. When the help flag is raised it removes the wrong typed keys replacing them
 // with the correct ones.
 func Start(keyboard keybd_event.KeyBonding) {
 	go keylogger.KeyLogger(pressedKeysChan, stopKeyloggerChan) // Start key-logging the keyboard
+	flushSliceTimer(resetTimerChan)
 
 	for key := range pressedKeysChan {
-		if key.KeyboardEvent.ScanCode == keybd_event.VK_F2 {
+		switch key.KeyboardEvent.ScanCode {
+		case keybd_event.VK_F2:
 			fmt.Println("controller: Re-Typing reason was reached (F2 button pressed), there is a need to Re-Type!")
 			stopKeyloggerChan <- true
 			typer.ReType(keyboard, keysPressed)                        // Start the Re-Typing process
 			go keylogger.KeyLogger(pressedKeysChan, stopKeyloggerChan) // Start key-logging the keyboard
 			keysPressed = keysPressed[:0]                              // Keeping the allocated memory
-		} else {
+
+		case keybd_event.VK_UP, keybd_event.VK_LEFT, keybd_event.VK_DOWN, keybd_event.VK_RIGHT,
+			keybd_event.VK_DELETE, keybd_event.VK_BACKSPACE, keybd_event.VK_TAB:
+			fmt.Println("controller: Special key was pressed, flushing the keysPressed slice.")
+			keysPressed = keysPressed[:0]
+
+		default:
+			resetTimerChan <- true
 			keysPressed = append(keysPressed, key)
 		}
 	}
+}
+
+// flushSliceTimer creates a timer, it waits for 5 seconds after the last key was pressed. When the time runs out
+// the method flushed the keysPressed slice.
+func flushSliceTimer(resetChan chan bool) {
+	timer := time.NewTimer(5 * time.Second) // Initial timer set to 5 seconds
+
+	go func() {
+		for {
+			select {
+			case <-timer.C:
+				fmt.Println("controller: Times out - Flushing the keysPressed slice.")
+				keysPressed = keysPressed[:0]
+				timer.Reset(5 * time.Second) // Reset the timer
+			case <-resetChan:
+				timer.Reset(5 * time.Second) // Reset the timer when the condition is met
+			}
+		}
+	}()
 }
